08fded2c035cf2683136912a582f4eb404be85f5
NullifyDev <nullydev@protonmail.com>
2025-02-20 10:27:36 +0000

    Added robots.txt

--- js/base.js
+++ js/base.js
@@ -15,7 +15,7 @@ let theme = document.createElement("img");
 		toggleTheme();
 		getTheme();
 	})
-	document.body.appendChild(theme);
+	document.getElementById("base").appendChild(theme);
 	getTheme();
 })();
 
@@ -31,22 +31,21 @@ function toggleTheme() {
 	localStorage.setItem("isLight", localStorage.getItem("isLight") == "1" ? 0 : 1);
 }
 
-function getTheme() {	
+function getTheme() {
 	if (localStorage.getItem("isLight") == 1) {
 		theme.src = "/media/img/sun.svg" 
 		document.documentElement.style.cssText = "--l0-bg-color: #ffffff; --l1-bg-color:#CCCCCC; --main-text-color: #000000; --faded-text-color: #AAAAAA"
+
+		let loc = window.location.href;
+		if (loc.endsWith("/pages/menu/plan.html"))    document.getElementById("plan").src = "../../media/img/plan.light.svg";
+		else if (loc.endsWith("/pages/aboutme.html")) document.getElementById("pfp").src = "../media/img/pfp.light.svg"
 	}
 	else {
 		theme.src = "/media/img/moon.svg" 
 		document.documentElement.style.cssText = "--l0-bg-color: #000000; --l1-bg-color: #232323; --main-text-color: #ffffff; --faded-text-color: #373737"
-	}
 
-	let loc = window.location.href;
-	if (loc.endsWith("/pages/menu/plan.html")) {
-		document.getElementById("plan").src = localStorage.getItem("isLight") == "1" ? 
-			"../../media/img/plan.light.svg" : "../../media/img/plan.dark.svg"
-	} else if (loc.endsWith("/pages/aboutme.html")) {
-		document.getElementById("pfp").src = localStorage.getItem("isLight") == "1" ? 
-			"../media/img/pfp.light.svg" : "../media/img/pfp.dark.svg"
+		let loc = window.location.href;
+		if (loc.endsWith("/pages/menu/plan.html"))    document.getElementById("plan").src = "../../media/img/plan.dark.svg";
+		else if (loc.endsWith("/pages/aboutme.html")) document.getElementById("pfp").src = "../media/img/pfp.dark.svg"
 	}
 }
